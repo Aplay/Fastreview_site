f31bf5b7e1406e7c7783617e4910a57ce20c56f0
Makarenok Roman <makarenok.roman@gmail.com>
2015-12-18 15:54:04 +0700

    up

--- protected/views/fastreview/_objects_view.php
+++ protected/views/fastreview/_objects_view.php
@@ -4,54 +4,38 @@ $themeUrl = '/themes/bootstrap_311/';
 
 $url = Yii::app()->createAbsoluteUrl('/fastreview/item', array( 'id'=>$data->id, 'dash'=>'-', 'themeurl'=>$data->category->url,'itemurl'=>$data->url));
 
-?>
-
- <a class="org_item" href="<?php echo $url; ?>">
- <div class="card">
- <div class="card-body card-padding p-t-15">
-<?php
-$im = '';
+$im = '/img/cap.gif';
 $alt = '';
 
  if($data->images){ 
-	
-	$im = $data->images[0]->getUrl('180x180','adaptiveResize','filename');
-    $alt = $data->title;
+  
+  $im = $data->images[0]->getUrl('180x180','adaptiveResize','filename');
+  $alt = $data->title;
 }
 ?>
-<div>
-<div class="org_item_title pull-left"><?php echo $data->title;  ?></div>
-
-<div class="clearfix"></div>
-</div>
-<div style="display:table">
-<?php if($im){ ?>
-  <div class="ologotip">
-  <img src="<?php echo $im; ?>" class="img-responsive" />
+<a class="oblects_view" href="<?php echo $url; ?>">
+<div class="media">
+    <div class="pull-left">
+            <img alt="" src="<?php echo $im; ?>" class="lv-img-lg" />
+        </div>
+        <div class="media-body m-t-5">
+        <p class="m-b-5 t-uppercase nocolor"><?php echo CHtml::encode($data->title);  ?></p>
+        <?php
+        if(!empty($data->description)){
+          echo '<p class="object_descr m-b-5">'.nl2br(CHtml::encode($data->description)).'</p>';
+        } ?>
+        
+        <div class="pull-left">
+        <!--<img alt="" src="/img/gud.png" />-->
+        <button type="button" class="btn btn-success btn-icon btn-icon waves-effect waves-circle waves-float finger-circle"><i class="fa fa-thumbs-up"></i></button>
+        </div>
+        <div class="pull-left p-l-10 c-6">
+          <div class="f-11">Местоположение</div>
+          <div class="f-10">Считают 56%</div>
+        </div>
+        </div>
   </div>
-  <?php } ?>
- <div class="org_info  <?php if($im){ echo ' with-pad'; } ?>" >
- 
-<ul>
-<?php 
-
-if($data->description){
-	echo '<li class="p-l-0 p-t-0 advert_descr">'.nl2br(CHtml::encode($data->description)).'</li>';
-}
-
-?>
-<li class="c-gray m-b-0 f-13 p-l-0 p-t-0" style="vertical-align:middle;">
-<span style="display:inline-block;vertical-align:middle;">
-<?php echo $data->category->title; ?></span>&nbsp;&nbsp; 
-<span style="font-size:20px;font-weight:300;vertical-align:middle;">|</span> &nbsp;&nbsp;<span style="display:inline-block;vertical-align:middle;"><?php echo Yii::app()->dateFormatter->format('d MMMM yyyy', $data->created_date); ?></span>
-</li>
-</ul>
- </div>
- </div>
- <div class="clearfix"></div>
- </div>
- </div>
- </a>
+</a>
  <div class="clearfix"></div>
 
 
