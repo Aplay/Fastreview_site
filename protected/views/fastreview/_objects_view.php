b18a95d349c48e1307358210e9b69c513a35601f
Makarenok Roman <makarenok.roman@gmail.com>
2015-12-22 17:09:53 +0700

    up

--- protected/views/fastreview/_objects_view.php
+++ protected/views/fastreview/_objects_view.php
@@ -25,14 +25,8 @@ if($data->images){
           echo '<p class="object_descr m-b-5">'.nl2br(CHtml::encode($data->description)).'</p>';
         } ?>
         
-        <div class="pull-left">
-        <!--<img alt="" src="/img/gud.png" />-->
-        <button type="button" class="btn btn-success btn-icon btn-icon waves-effect waves-circle waves-float finger-circle"><i class="fa fa-thumbs-up"></i></button>
-        </div>
-        <div class="pull-left p-l-10 c-6">
-          <div class="f-11">Местоположение</div>
-          <div class="f-10">Считают 56%</div>
-        </div>
+         <?php  $this->widget('application.modules.poll.widgets.Poll', array('org_id'=>$data->id)); ?>
+        
         </div>
   </div>
 </a>
