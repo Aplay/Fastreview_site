3e4351059e04b84c99cc0b9bf22ce7ec81fc9ebf
Makarenok Roman <makarenok.roman@gmail.com>
2015-12-22 16:25:24 +0700

    up

--- protected/modules/poll/widgets/views/poll.php
+++ protected/modules/poll/widgets/views/poll.php
@@ -9,11 +9,13 @@ $diff = $choice->yes - $choice->no;
 		}
 	}
 ?>
-<div class="pull-left">
+<div style="display:table;">
+<div style="display:table-cell;vertical-align:middle;">
 <button type="button" class="btn btn-success btn-icon btn-icon waves-effect waves-circle waves-float finger-circle"><i class="fa fa-thumbs-up"></i></button>
 </div>
-<div class="pull-left p-l-10 c-6">
+<div style="display:table-cell;vertical-align:middle;" class="p-l-10 c-6">
 <div class="f-11"><?php echo CHtml::encode($choice->label); ?></div>
 <div class="f-10"><?php echo $weight; ?></div>
+</div>
 </div>
 <?php } ?>
