c5a2dfcee3ca349629794bff2452f1cb8be95934
Makarenok Roman <makarenok.roman@gmail.com>
2015-12-24 15:28:40 +0700

    up

--- protected/views/pages/about.php
+++ protected/views/pages/about.php
@@ -1,5 +1,5 @@
  <div style="font-size:20px;font-weight:300;margin-left:26px;margin-top:80px;margin-bottom:18px;" class="rootCategory">
-О нас
+В разработке
 </div>
 <div class="card">
 <div class="card-body card-padding">
