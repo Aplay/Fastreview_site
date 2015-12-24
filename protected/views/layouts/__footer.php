1d51e798eddf7071e81712c6aeed8a7c7069c352
Makarenok Roman <makarenok.roman@gmail.com>
2015-12-24 15:34:01 +0700

    up

--- protected/views/layouts/__footer.php
+++ protected/views/layouts/__footer.php
@@ -2,9 +2,3 @@
 $currentUrl = Yii::app()->getRequest()->getHostInfo().Yii::app()->getRequest()->getUrl();
 $this->widget('ext.widgets.Login', array('return_url' => $currentUrl)); 
 ?>
-<div
-  class="fb-like"
-  data-share="true"
-  data-width="450"
-  data-show-faces="true">
-</div>
