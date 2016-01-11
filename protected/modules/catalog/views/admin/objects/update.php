39189a289e01b5e6b068fe4059da3bd84f1015ed
Makarenok Roman <makarenok.roman@gmail.com>
2016-01-11 09:41:01 +0700

    add link to object from admin

--- protected/modules/catalog/views/admin/objects/update.php
+++ protected/modules/catalog/views/admin/objects/update.php
@@ -5,7 +5,11 @@
 
 ?>
 <div style="margin-bottom:15px;">
+<?php 
+$siteUrl = Yii::app()->createAbsoluteUrl('/fastreview/item', array( 'id'=>$model->id, 'dash'=>'-', 'itemurl'=>$model->url));
 
+echo CHtml::link('Смотреть на сайте',$siteUrl,array('target'=>'_blank','style'=>'display:inline-block;margin-left:40px;')); 
+?>
 </div>
 <?php } ?>
 <?php $this->renderPartial('_form', array(
